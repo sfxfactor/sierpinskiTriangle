1e01130e95644501ac944786434c0af4919a58b1
Samuel Factor <sfactor@astro.as.utexas.edu>
2017-07-23 15:02:52 -0500

    save intermediate figs for gif, ignore images

--- pltTriangle.py
+++ pltTriangle.py
@@ -9,18 +9,24 @@ pt2=[0.5,np.sqrt(3.)/2.]
 verts=np.array([pt0,pt1,pt2])
 
 plt.plot(verts[:,0],verts[:,1],'bo')
+
+plt.savefig('tri{:05d}.png'.format(0))
+
 ptc=pt1
 ptsx=np.array(ptc[0])
 ptsy=np.array(ptc[1])
 
 n=int(sys.argv[1])
-for i in range(n):
+for i in range(1,n):
   if i%(n/10)==0:
       print (i*100./n)
   r=np.random.randint(3) 
   ptc=(ptc+verts[r])/2.
   ptsx=np.append(ptsx,ptc[0])
   ptsy=np.append(ptsy,ptc[1])
+  plt.plot(ptc[0],ptc[1],'r,')
+  plt.savefig('tri{:05d}.png'.format(i))
+
 
 plt.plot(ptsx,ptsy,'r,')
 plt.savefig('points.pdf')
